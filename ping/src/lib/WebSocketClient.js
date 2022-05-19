39097060a86692e10f16881b0d431fe2b528ba06
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-05-20 00:32:39 +0530

    handle client disconnection to wss

--- ping/src/lib/WebSocketClient.js
+++ ping/src/lib/WebSocketClient.js
@@ -5,6 +5,10 @@ export class WebSocketClient {
         this.createWebSocket();
     }
 
+    /**
+     * Create new web socket connection
+     * @returns {void}
+     */
     createWebSocket() {
         this.ws = new WebSocket('ws://localhost:4000/');
         this.ws.onopen = (evt) => {
@@ -12,14 +16,33 @@ export class WebSocketClient {
         }
         this.ws.onmessage = (evt) => {
             this.onMessage(evt);
+        }        
+        this.ws.onclose = (evt) => {
+            this.onClose(evt);
         }
     }
 
+    /**
+     * Handles new connection opened
+     * @returns {void}
+     */
     onOpen(evt) { 
         console.log('WebSocket Client Connected');
         this.ws.send('Hi this is web client.');
     }
 
+    /**
+     * Handles disconnetion to the web socket
+     * @returns {void}
+     */
+    onClose(evt) { 
+        console.log('WebSocket Client Disconnected');
+    }
+
+    /**
+     * Handles messages from the web socket
+     * @returns {void}
+     */
     onMessage(evt) {
         console.log("Received: '" + evt.data + "'");
     }
